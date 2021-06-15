49f2218aea47790d1b5997243cf304795662e171
Constanza <constanzab.vega@gmail.com>
2021-06-15 19:42:54 -0400

    fifth OOP activity added

---
+++ python/OOP/modularizing/parent.py
@@ -0,0 +1,28 @@
+local_val = "magical unicorns"
+
+
+def square(x):
+    return x * x
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+
+    def say_hello(self):
+        return "hello"
+
+
+# in the same file, add the following below the User class
+print(square(5))
+user = User("Anna")
+print(user.name)
+print(user.say_hello())
+print(__name__)
+
+if __name__ == "__main__":
+    print("El archivo esta siendo ejecutado directamente")
+else:
+    print(". El archivo se llama: ", __name__)
+
+print(__name__)
